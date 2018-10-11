203a19d6fb03c3fb953cd285964b9b22883cdbe8
Luka Devic <43738975+LukaDevic@users.noreply.github.com>
2018-10-11 20:19:28 +0200

    Update Customer.cs

--- MultipleApplications/Models/Customer.cs
+++ MultipleApplications/Models/Customer.cs
@@ -27,13 +27,3 @@ namespace MultipleApplications.Models
 }
 
 
-//Sql(@"
-//INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'a8f216e0-c4d5-400e-95d7-fe686b2e4c54', N'lukadevic88@gmail.com', 0, N'AB0ovo+i8atgaWCQEPWwHfC4O9GXvTz+LuWu1q2ZWZwGvhb57TPe7Bvq56bLuH6t3w==', N'490b101a-8c5d-44e1-bcc5-6414cccf5830', NULL, 0, 0, NULL, 1, 0, N'lukadevic88@gmail.com')
-//INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'e90e6a99-05bd-458a-832f-a2b4c75ed4f7', N'raskovlj@gmail.com', 0, N'AKm/t9aIj0hn6wLFczaTXk/FPmyDd7xgL3YXQUbOZwOTL4xD+QvDGMhqtRGdokGj3Q==', N'd5eded0d-1593-4d84-a571-ee7a8552d81c', NULL, 0, 0, NULL, 1, 0, N'raskovlj@gmail.com')
-
-//INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'9d814741-88a4-45be-866b-369f3c20ba87', N'CanUseTheApplication')
-
-//INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'a8f216e0-c4d5-400e-95d7-fe686b2e4c54', N'9d814741-88a4-45be-866b-369f3c20ba87')
-//INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'e90e6a99-05bd-458a-832f-a2b4c75ed4f7', N'9d814741-88a4-45be-866b-369f3c20ba87')
-
-//");
